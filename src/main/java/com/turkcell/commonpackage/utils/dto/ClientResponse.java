18d1b5804ac08b0f4406f05174f5837527b3b8fc
Yasin <yasinzc75@gmail.com>
2024-05-27 13:15:06 +0300

    first commit

---
+++ src/main/java/com/turkcell/commonpackage/utils/dto/ClientResponse.java
@@ -0,0 +1,14 @@
+package com.turkcell.commonpackage.utils.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientResponse {
+    private boolean isSuccess;
+}
